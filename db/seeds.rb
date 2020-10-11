492d34ef2d1f546dfafb056a64bc65278ee394f4
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-10-11 19:43:16 -0400

    mig

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
+
+
 Stock.create(ticker: "AAPL", price: 119, name: "Apple Inc")
 Stock.create(ticker: "TSL", price: 119, name: "Tesla Inc")
 Stock.create(ticker: "AMZ", price: 119, name: "Amazon Inc")
+
+
+
 
 User.create(username:"nashatchy", email:"n@g.com", password: "123456")
 User.create(username:"shompa", email:"s@g.com", password: "123456")
